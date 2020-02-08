0b47e45605822aa92c092dd4a1eeb017c7fb5871
camalagarane <shobana.camal05@gmail.com>
2020-02-08 18:56:20 +0100

    react working

---
+++ resources/js/components/Header/Header.js
@@ -0,0 +1,13 @@
+// resources/assets/js/components/Header.js
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button';
+
+const Header = () => (
+    <Button variant="contained" color="primary">
+        Hello World
+    </Button>
+)
+
+export default Header
